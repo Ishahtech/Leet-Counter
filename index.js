5f8bffdeae76a194756e3fbfec29364f4af671d0
[ishahtech] <[salawudeenaishatolabisi@gmail.com]>
2023-10-17 13:36:09 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// let saveEl = document.getElementById("save-el")
+// let countEl = document.getElementById("count-el")
+// let count = 0
+
+
+// function increment() {
+//     count += 1
+//     countEl.textContent = count
+// }
+
+// function save() {
+//     countStr = count + " - "
+//     saveEl.textContent += countStr
+//     countEl.textContent = 0
+//     count = 0
+   
+// }
+
+
+let countRegion = document.getElementById("count-region")
+
+let saveRegion = document.getElementById("save-region")
+let count = 0 
+
+function increment()   {
+    count += 1
+    countRegion.textContent = count
+}
+
+
+function save() {
+    countString = count + " - "
+    saveRegion.textContent += countString
+    countRegion.textContent = 0
+    count = 0
+}
